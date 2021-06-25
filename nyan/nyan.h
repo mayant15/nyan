1e99e0d7087700fd88cdb605e46498018da7ea59
Mayant Mukul <mayantmukul@outlook.in>
2021-06-25 22:20:06 +0530

    Add monoids and test for string concatenation

--- nyan/nyan.h
+++ nyan/nyan.h
@@ -2,6 +2,15 @@
 
 #include <functional>
 
+#define NYAN_DECLARE_MONOID(TYPE, IDENTITY, OPERATOR) \
+template<> struct nyan::Monoid<TYPE> { \
+    const static inline TYPE identity = IDENTITY; \
+    static TYPE operate(TYPE x, TYPE y) { \
+        constexpr auto func = OPERATOR; \
+        return std::invoke(func, x, y); \
+    } \
+};
+
 namespace nyan
 {
     template<class OuterFunc, class InnerFunc>
@@ -9,4 +18,14 @@ namespace nyan
     {
         return [=](const auto x) { return outer(inner(x)); };
     }
+
+    template<class Type>
+    struct Monoid
+    {
+        const static inline Type identity = Type {};
+        static Type operate(Type x, Type y) 
+        {
+            return Type {};
+        }
+    };
 }
--- tests/doctest.cpp
+++ tests/doctest.cpp
@@ -2,3 +2,4 @@
 #include "doctest.h"
 
 #include "compose.h"
+#include "monoid.h"
